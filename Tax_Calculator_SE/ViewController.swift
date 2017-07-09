c57e257577ae6588aec3317ea80196a3f66ba675
ula1990 <uladzislau.daratsiuk@gmail.com>
2017-07-09 22:54:13 +0200

    Initial Commit

---
+++ Tax_Calculator_SE/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tax_Calculator_SE
+//
+//  Created by Ulad Daratsiuk-Demchuk on 7/9/17.
+//  Copyright © 2017 Ulad Daratsiuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
