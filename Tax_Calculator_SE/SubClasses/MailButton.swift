0c1e259eb322febaf0ff87ea5bff791b3814def1
ula1990 <uladzislau.daratsiuk@gmail.com>
2018-03-27 12:29:57 +0200

    Cleaning

--- Tax_Calculator_SE/SubClasses/MailButton.swift
+++ Tax_Calculator_SE/SubClasses/MailButton.swift
@@ -32,12 +32,4 @@ class MailButton: UIButton {
         }
     }
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
 }
